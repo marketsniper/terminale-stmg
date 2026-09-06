d95c16ab5d8249540c7c1d8896cc499afc8fbb37
marketsniper <ilanbertin2008@gmail.com>
2026-09-06 15:16:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stmg-v2';
+const CACHE = 'stmg-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
