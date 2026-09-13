18509de7d4912692a716dbad4cf58d53bbedabeb
Ilan Bertin <detente.epil@gmail.com>
2026-09-13 14:22:53 +0200

    Cache auto-versionné par build + bandeau nouvelle version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stmg-v6';
+const CACHE = 'stmg-11458cc4fa';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
