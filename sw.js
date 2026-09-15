a88d468ffe61684c8e4882880cb367c5cec0f2f1
Ilan Bertin <detente.epil@gmail.com>
2026-09-15 18:01:43 +0200

    GF, Management et SDGN alignés sur les dossiers élève du lycée (Teams/SharePoint)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stmg-deea5c4999';
+const CACHE = 'stmg-d4ae58d9e0';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
