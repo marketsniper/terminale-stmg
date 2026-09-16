2f6aae43ec9b5751ff747b4fcb5a2c5a8afbca4e
Ilan Bertin <detente.epil@gmail.com>
2026-09-16 18:04:19 +0200

    Terminale : audit ARBS (droit, éco, GF), tableaux définition/exemple/distinction, coûts partiels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stmg-b62c43e75e';
+const CACHE = 'stmg-54af9c0d1c';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
