d12fb1211bb971843026c7e634408de9a1b6ec6e
Ilan Bertin <detente.epil@gmail.com>
2026-09-20 13:19:47 +0200

    Mode DS : ne propose que les matières du profil (inerte en édition perso)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stmg-342cce4f06';
+const CACHE = 'stmg-126e0116fa';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
