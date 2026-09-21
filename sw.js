5a9fcfdc1643594eedaea5d36ce23159729d70dd
Ilan Bertin <detente.epil@gmail.com>
2026-09-21 22:01:23 +0200

    Cours de Mme Francis : gf-1 complétée (sécurité, sauvegarde), mgmt-1 complétée (Maslow, marché, 4P, veille), nouvelle fiche mgmt-1b (modèle économique)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stmg-22e79e7982';
+const CACHE = 'stmg-17ff18985e';
 //  Plusieurs apps cohabitent sur le domaine (perso à la racine, Maths dans /maths/, version partagée dans
 //  son propre dossier). Chacune ne nettoie QUE ses caches (même préfixe) et ne sert QUE son dossier.
 const PREFIX = CACHE.slice(0, CACHE.lastIndexOf('-') + 1);
