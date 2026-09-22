09515af08f9190927b9e816add8e8fd66c80962d
Ilan Bertin <detente.epil@gmail.com>
2026-09-22 16:36:25 +0200

    Mise à jour

--- ami-34e6e3/sw.js
+++ ami-34e6e3/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'amistmg-7313fcc53f';
+const CACHE = 'amistmg-decaab5c5b';
 //  Plusieurs apps cohabitent sur le domaine (perso à la racine, Maths dans /maths/, version partagée dans
 //  son propre dossier). Chacune ne nettoie QUE ses caches (même préfixe) et ne sert QUE son dossier.
 const PREFIX = CACHE.slice(0, CACHE.lastIndexOf('-') + 1);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stmg-ab032e8164';
+const CACHE = 'stmg-3d9419e781';
 //  Plusieurs apps cohabitent sur le domaine (perso à la racine, Maths dans /maths/, version partagée dans
 //  son propre dossier). Chacune ne nettoie QUE ses caches (même préfixe) et ne sert QUE son dossier.
 const PREFIX = CACHE.slice(0, CACHE.lastIndexOf('-') + 1);
