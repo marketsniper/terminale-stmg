43bf74c2eaeef8773a31ca0e55f48d70a366e546
Ilan Bertin <detente.epil@gmail.com>
2026-09-22 17:03:07 +0200

    Mise à jour

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stmg-3d9419e781';
+const CACHE = 'stmg-b0488adcb3';
 //  Plusieurs apps cohabitent sur le domaine (perso à la racine, Maths dans /maths/, version partagée dans
 //  son propre dossier). Chacune ne nettoie QUE ses caches (même préfixe) et ne sert QUE son dossier.
 const PREFIX = CACHE.slice(0, CACHE.lastIndexOf('-') + 1);
